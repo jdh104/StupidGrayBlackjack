432da40aea6361da6e1cc2b8dd3a638253dd1a2f
Jonah <jdh104@latech.edu>
2018-10-24 13:15:37 -0500

    Finish structural work on StupidConnector class.

--- Implementation/StupidBlackjackSln/Code/StupidConnector.cs
+++ Implementation/StupidBlackjackSln/Code/StupidConnector.cs
@@ -6,20 +6,24 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
+using StupidBlackjackSln.Code.StupidServer;
+
 namespace StupidBlackjackSln.Code
 {
     class StupidConnector
     {
         private TcpClient client;
         private NetworkStream netstream;
-        private String serverDomain = "";
-        private int serverPort = 0;
+        private String serverDomain;
+        private int serverPort;
 
         /// <summary>
         /// Create a new connection to the default matchmaking server.
         /// </summary>
         public Connector()
         {
+            this.serverDomain = StupidServer.DEFAULT_DOMAIN;
+            this.serverPort = StupidServer.DEFAULT_PORT;
             this.client = GenerateTcpClient(serverDomain, serverPort);
             this.netstream = client.GetStream();
         }
@@ -34,6 +38,12 @@ namespace StupidBlackjackSln.Code
             this.serverDomain = domain;
             this.serverPort = port;
             this.client = GenerateTcpClient(serverDomain, serverPort);
+            this.netstream = client.GetStream();
+        }
+
+        public void Close() {
+            netstream.Close();
+            client.Close();
         }
 
         /// <summary>
@@ -44,17 +54,32 @@ namespace StupidBlackjackSln.Code
         /// <returns></returns>
         private TcpClient GenerateTcpClient(String domain, int port)
         {
-            IPAddress serverIp = Dns.GetHostEntry(domain).AddressList[0];
-            //TODO
+            //IPAddress serverIp = Dns.GetHostEntry(domain).AddressList[0];
+            return new TcpClient(domain, port);
+        }
+
+        /// <summary>
+        /// Alias for Encoding.ASCII.GetString() to make code more readable.
+        /// </summary>
+        /// <param name="b">Byte array to convert</param>
+        /// <returns>The string represented by the given byte array</return>
+        private String GetStringFromBytes(byte[] b) {
+            return Encoding.ASCII.GetString(b, 0, b.Length());
         }
 
         /// <summary>
         /// Fetch a list of the games currently being hosted on the matchmaking server.
         /// </summary>
         /// <returns>An array of strings containing game names and id's</returns>
-        public String[] FetchListOfGames()
+        public String FetchListOfGames()
         {
-            return null;
+            this.sendString("list");
+
+            byte[] buffer = new byte[40];
+            netstream.Read(buffer, 0, buffer.Length());
+            buffer = new byte[Int32.Parse(GetStringFromBytes(buffer))];
+            netstream.Read(buffer, 0, buffer.Length());
+            return GetStringFromBytes(buffer);
         }
 
         /// <summary>
@@ -64,7 +89,10 @@ namespace StupidBlackjackSln.Code
         /// <returns>The generated id number for the game.</returns>
         public int HostNewGame(String serverName)
         {
-            return 0;
+            byte[] buffer = new byte[StupidServer.ID_SIZE_IN_BYTES];
+            this.sendString("new");
+            netstream.Read(buffer, 0, buffer.Length());
+            return Int32.Parse(GetStringFromBytes(buffer));
         }
 
         /// <summary>
@@ -73,7 +101,7 @@ namespace StupidBlackjackSln.Code
         /// <param name="id">id of game to join</param>
         public void JoinGameByID(int id)
         {
-
+            this.sendString("join " + id);
         }
 
         /// <summary>
@@ -82,11 +110,13 @@ namespace StupidBlackjackSln.Code
         /// <param name="id">The id of the game to remove</param>
         public void RemoveHostedGame(int id)
         {
-
+            this.sendString("rm " + id);
         }
 
-        private void sendString(String s) {
-
+        private void sendString(String s)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(s);
+            netstream.Write(data, 0, data.Length());
         }
     }
 }
---
+++ Implementation/StupidBlackjackSln/Code/StupidServer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidBlackjackSln.Code {
+    class StupidServer {
+
+        public static const int ID_SIZE_IN_BYTES = 0;
+        public static const int DEFAULT_PORT = 0;
+        public static const String DEFAULT_DOMAIN = "";
+
+        public StupidServer() {
+            
+        }
+    }
+}
