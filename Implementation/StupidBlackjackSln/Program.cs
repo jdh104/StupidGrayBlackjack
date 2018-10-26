4ce02776d4355ad9e575cd2dd5fef844e99c7a89
Jonah <jdh104@latech.edu>
2018-10-26 12:10:00 -0500

    Add StupidServer singleton to Program.cs

--- Implementation/StupidBlackjackSln/Program.cs
+++ Implementation/StupidBlackjackSln/Program.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 
 namespace StupidBlackjackSln {
   static class Program {
+
+    private static StupidServer server = null;
+
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
@@ -14,6 +17,32 @@ namespace StupidBlackjackSln {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new frmTitle());
+    }
+
+    /// <summary>
+    /// Close the singleton StupidServer if it exists.
+    /// </summary>
+    public static void CloseStupidServer() {
+        if (server != null) {
+          server.Close();
+          server = null;
+        }
+    }
+
+    /// <summary>
+    /// Get singleton instance of StupidServer.
+    /// </summary>
+    /// <returns> The singleton instance of StupidServer</returns>
+    public static StupidServer GetServer() {
+      return server;
+    }
+
+    /// <summary>
+    /// Start a singleton StupidServer
+    /// </summary>
+    public static void StartStupidServer() {
+      server = new StupidServer();
+      server.Start();
     }
   }
 }
