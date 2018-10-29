8c9305bb18f8ff2af1a84365b67944107f61c031
BenToups <bit001@latech.edu>
2018-10-28 19:09:59 -0500

    Fix my mistake cuz I'm dumb

---
+++ Implementation/StupidBlackjackSln/FrmServer.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace StupidBlackjackSln
+{
+	partial class FrmServer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+			this.CloseServer = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// fileSystemWatcher1
+			// 
+			this.fileSystemWatcher1.EnableRaisingEvents = true;
+			this.fileSystemWatcher1.SynchronizingObject = this;
+			// 
+			// CloseServer
+			// 
+			this.CloseServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CloseServer.Location = new System.Drawing.Point(653, 12);
+			this.CloseServer.Name = "CloseServer";
+			this.CloseServer.Size = new System.Drawing.Size(135, 46);
+			this.CloseServer.TabIndex = 0;
+			this.CloseServer.Text = "Close";
+			this.CloseServer.UseVisualStyleBackColor = true;
+			this.CloseServer.Click += new System.EventHandler(this.CloseServer_Click);
+			// 
+			// FrmServer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.CloseServer);
+			this.Name = "FrmServer";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.IO.FileSystemWatcher fileSystemWatcher1;
+		private System.Windows.Forms.Button CloseServer;
+	}
+}
---
+++ Implementation/StupidBlackjackSln/FrmServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StupidBlackjackSln
+{
+	public partial class FrmServer : Form
+	{
+		public FrmServer()
+		{
+			InitializeComponent();
+		}
+
+		private void CloseServer_Click(object sender, EventArgs e)
+		{
+			frmTitle dicks = new frmTitle();
+			dicks.Show();
+			this.Hide();
+			Program.CloseStupidServer();
+		}
+	}
+}
