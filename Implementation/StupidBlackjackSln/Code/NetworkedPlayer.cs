adef94b3f15e827e44424fdc933e7c76a3357bd3
Jonah <jdh104@latech.edu>
2018-10-31 18:27:40 -0500

    Add classmaster headers to Jonah's files

--- Implementation/StupidBlackjackSln/Code/NetworkedPlayer.cs
+++ Implementation/StupidBlackjackSln/Code/NetworkedPlayer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//ClassMaster: Jonah?
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,7 @@ namespace StupidBlackjackSln.Code
     {
         public NetworkedPlayer()
         {
+
         }
     }
 }
--- Implementation/StupidBlackjackSln/Code/StupidServer.cs
+++ Implementation/StupidBlackjackSln/Code/StupidServer.cs
@@ -1,3 +1,5 @@
+//ClassMaster: Jonah
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
