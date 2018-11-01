43fedb438fffe1db618aa89d7d572e76b859d9d2
Jeremy Thomas <Mr.JJNigma@yahoo.com>
2018-11-01 13:54:29 -0500

    Implemented a timer not yet connected to anything.

--- Implementation/StupidBlackjackSln/FrmNewGame.Designer.cs
+++ Implementation/StupidBlackjackSln/FrmNewGame.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picPlayerCard1 = new System.Windows.Forms.PictureBox();
             this.picPlayerCard2 = new System.Windows.Forms.PictureBox();
             this.picPlayerCard3 = new System.Windows.Forms.PictureBox();
@@ -37,6 +38,8 @@
             this.btnStand = new System.Windows.Forms.Button();
             this.lblPlayerScore = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picPlayerCard1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPlayerCard2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPlayerCard3)).BeginInit();
@@ -137,12 +140,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 37);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // FrmNewGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::StupidBlackjackSln.Properties.Resources.greenfelt;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblPlayerScore);
             this.Controls.Add(this.btnStand);
@@ -177,5 +196,7 @@
         private System.Windows.Forms.Button btnStand;
         private System.Windows.Forms.Label lblPlayerScore;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
