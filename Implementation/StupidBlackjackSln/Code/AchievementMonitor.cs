52f791fddbd2f0c151bfd27d72c1e6bfdb4c4981
Dakota Digilormo <kota097@gmail.com>
2018-11-02 18:04:49 -0500

    Implemented Singleton and Lazy Initialization patterns to AchievementMonitor

--- Implementation/StupidBlackjackSln/Code/AchievementMonitor.cs
+++ Implementation/StupidBlackjackSln/Code/AchievementMonitor.cs
@@ -10,11 +10,21 @@ namespace StupidBlackjackSln.Code
 {
     class AchievementMonitor
     {
+        private static AchievementMonitor instance = null;
         public List<Achievement> achievements = null;
 
-        public AchievementMonitor()
+        private AchievementMonitor()
         {
             achievements = new List<Achievement>();
+        }
+
+        public static AchievementMonitor getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new AchievementMonitor();
+            }
+            return instance;
         }
 
         public void addAchievement(String name, String description, String iconName)
